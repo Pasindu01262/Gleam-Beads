74d116ecab329d6493cdc84702124396186909dd
pasindu Ransika <pasindukossinna@gmail.com>
2026-03-02 15:36:43 +0845

    Refactor CartItem component for improved clarity

--- src/Component/CartItem/CartItem.jsx
+++ src/Component/CartItem/CartItem.jsx
@@ -3,10 +3,9 @@ import "./CartItem.css"
 import { ShopContext } from '../../Contex/ShopContext'
 import remove_icon from "../Assest/Frontend_Assets/cart_cross_icon.png"
 
-
 function CartItem() {
 
-    const {getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
+    const { getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
 
     return (
         <div className='cartitem'>
@@ -15,29 +14,37 @@ function CartItem() {
                 <p>Title</p>
                 <p>Price</p>
                 <p>Quantity</p>
+                <p>Total</p>
                 <p>Remove</p>
-                     
             </div>
             <hr />
 
             {all_product.map((e) => {
-                if (cartItems[e.id]==0) {
+                // ✅ Only show items with quantity greater than 0
+                if (cartItems[e.id] > 0) {
                     return (
                         <div key={e.id}>
                             <div className='cartitem-format cartitems-from-main'>
-                                <img src={e.image} alt='' className='carticon-product-icon' />
+                                <img
+                                    src={e.image}
+                                    alt={e.name}
+                                    className='carticon-product-icon'
+                                />
 
                                 <p>{e.name}</p>
+
                                 <p className='new-price'>${e.new_price}</p>
 
                                 <button className='carticon-quantity'>
                                     {cartItems[e.id]}
                                 </button>
 
-                                <p>${e.new_price * cartItems[e.id]}</p>
+                                <p>
+                                    ${e.new_price * cartItems[e.id]}
+                                </p>
 
                                 <img
-                                   className='carditem-removeicon'
+                                    className='cartitem-removeicon'
                                     src={remove_icon}
                                     onClick={() => removeFromCart(e.id)}
                                     alt='remove'
@@ -52,29 +59,34 @@ function CartItem() {
 
             <div className='cartitem-down'>
                 <div className='cartitem-total'>
-                    <h1>cart Totals</h1>
-                    <div>
-                        <div className='cartitem-total-item'>
-                            <p>subtatal</p>
-                            <p>${getTotalCartAmount()}</p>
-                        </div>
-                        <hr/>
-                        <div className='cartitems-total-item'>
-                            <p>Delivery Fee</p>
-                            <p>Free</p>
-                        </div>
-                        <hr/>
-                        <div className='cartitems-total-item'>
-                            <h3>Total</h3>
-                            <h3>${getTotalCartAmount()}</h3>
-                        </div>
+                    <h1>Cart Totals</h1>
+
+                    <div className='cartitem-total-item'>
+                        <p>Subtotal</p>
+                        <p>${getTotalCartAmount()}</p>
                     </div>
+
+                    <hr />
+
+                    <div className='cartitem-total-item'>
+                        <p>Delivery Fee</p>
+                        <p>Free</p>
+                    </div>
+
+                    <hr />
+
+                    <div className='cartitem-total-item'>
+                        <h3>Total</h3>
+                        <h3>${getTotalCartAmount()}</h3>
+                    </div>
+
                     <button>PROCEED TO CHECKOUT</button>
                 </div>
+
                 <div className='cartitem-promocode'>
-                    <p>If you have a promo code, Enter it here </p>
+                    <p>If you have a promo code, enter it here</p>
                     <div className='cartitem-promobox'>
-                        <input type='text'placeholder='promo code'/>
+                        <input type='text' placeholder='Promo code' />
                         <button>Submit</button>
                     </div>
                 </div>
@@ -85,3 +97,4 @@ function CartItem() {
 }
 
 export default CartItem;
+
